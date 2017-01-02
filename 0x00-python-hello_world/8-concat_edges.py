c01dab30a95eba1ff516f57e55e5a4256eadd903
philipyoo <philipyoo10@gmail.com>
2017-01-02 19:53:34 +0000

    Add solution for 0x00-8

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:67] + str[107:112] + str[:6])
