a40438a0bb9df225587166eccbdff2f95700a1e0
philipyoo <philipyoo10@gmail.com>
2017-01-03 23:09:10 +0000

    Add solution for 0x01-8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    tmp = list(str)
+    for i in range(len(tmp)):
+        if (ord(tmp[i]) > 96 and ord(tmp[i]) < 123):
+            tmp[i] = chr(ord(tmp[i]) - 32)
+    print(("").join(tmp))
