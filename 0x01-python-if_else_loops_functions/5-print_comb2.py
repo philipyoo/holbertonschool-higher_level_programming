bdb2134822dba6fa8413115b39fe11b93887f234
philipyoo <philipyoo10@gmail.com>
2017-01-03 21:56:34 +0000

    Edit solution for 0x01-5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if (i < 10):
-        print("0", end="")
-    print("{}".format(i), end="")
-    if (i != 99):
-        print(", ", end="")
-print("")
+print(", ".join(str(i).zfill(2) for i in range(100)))
