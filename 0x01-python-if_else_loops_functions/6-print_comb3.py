f3062e6492c531daddddc203b8825af5a14eda87
philipyoo <philipyoo10@gmail.com>
2017-01-03 22:13:15 +0000

    Add solution for 0x01-6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(i + 1, 10):
+        print(str(i) + str(j), end="")
+        if int(str(i) + str(j)) < 89:
+            print(", ", end="")
+print("");
