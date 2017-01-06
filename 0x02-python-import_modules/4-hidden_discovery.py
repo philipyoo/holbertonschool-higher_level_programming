3d88cb8e5dc913a13407ee60298574b541467507
philipyoo <philipyoo10@gmail.com>
2017-01-06 05:05:26 +0000

    Add solution for 0x02-4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys, hidden_4
+    all_dir = dir(hidden_4)
+    avoid = "__"
+    for i in range(0, len(all_dir)):
+        if avoid not in all_dir[i]:
+            print(all_dir[i])
