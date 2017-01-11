1249822a239e7499641b3a6fd045d99e9c38909c
philipyoo <philipyoo10@gmail.com>
2017-01-11 01:19:58 +0000

    Edit solution for 0x03-8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence != None:
+    if len(sentence) > 0:
         return tuple([len(sentence), sentence[0]])
+    else:
+        return tuple([0, None])
