953dd5a4a87a586f1fc73a2ab9679a77f12655ca
philipyoo <philipyoo10@gmail.com>
2017-01-12 22:30:54 +0000

    Edit solution for 0x04-0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [list(map(lambda n: n**2, sublist)) for sublist in matrix]
-    return new_matrix
+    return [list(map(lambda n: n**2, sublist)) for sublist in matrix]
