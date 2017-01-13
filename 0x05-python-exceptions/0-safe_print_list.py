73f91ead8c07caeec0d62ad06acb48955df24105
philipyoo <philipyoo10@gmail.com>
2017-01-13 21:09:09 +0000

    Add solution for 0x05-0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+    except IndexError:
+        return i
+    else:
+        return x
+    finally:
+        print("")
