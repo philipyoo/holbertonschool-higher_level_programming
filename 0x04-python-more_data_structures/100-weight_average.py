dc0c7f6fdd816117d0a6dbf91d30974612955b29
philipyoo <philipyoo10@gmail.com>
2017-01-13 00:09:04 +0000

    Edit solution for 0x04-12

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if len(my_list) is 0:
+        return 0
     a = list(map(list, zip(*my_list)))
     b = [x * y for x, y in zip(a[0], a[1])]
     return sum(b) / sum(a[1])
