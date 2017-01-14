b7180c7da557a081af3d642a59c9835e7d3fa163
philipyoo <philipyoo10@gmail.com>
2017-01-14 00:37:19 +0000

    Add solution for 0x05-7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as err:
-        print("Exception: {}", err)
+        print("Exception: {}".format(err), file=sys.stderr)
         return False
     else:
         return True
