f08ac8ab72dbc5fd3db98a11fa66da6ce3f7ef68
philipyoo <philipyoo10@gmail.com>
2017-01-18 01:03:22 +0000

    Add tests for 0x06-1, add function file

---
+++ 0x06-python-test_driven_development/1-mul.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is the "Multiplication" module.
+
+The Multiplication module supplies a simple function \
+to multiply 2 Python variables.
+"""
+
+
+def mul(a, b):
+    """Return the multiplication of 2 arguments
+    """
+    return a * b
