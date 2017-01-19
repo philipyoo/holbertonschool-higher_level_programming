0615cb3dfab4c9345191dd6ca799f7c87384ac2b
philipyoo <philipyoo10@gmail.com>
2017-01-19 00:57:31 +0000

    Add comments to 0x06-3

--- 0x06-python-test_driven_development/3-say_my_name.py
+++ 0x06-python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,15 @@
+#!/usr/bin/python3
+"""
+This is the "Say My Name" module.
+
+The Say My Name module takes one required parameter and one optional.
+It prints "My name is (first) (last)" where (first) and (last) are the args.
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """Print My name is (first) (last) if given, else print error.
+    """
     try:
         if not isinstance(first_name, str):
             raise TypeError("first_name must be a string")
