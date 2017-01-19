c32fbc717be8ce2527a22e6b56c9c05fad4c535b
philipyoo <philipyoo10@gmail.com>
2017-01-19 01:11:43 +0000

    Edit solution for 0x06-4, add comments

--- 0x06-python-test_driven_development/4-print_square.py
+++ 0x06-python-test_driven_development/4-print_square.py
@@ -1,15 +1,21 @@
+"""
+This is the "Print Square" module.
+
+The Print Square module prints a square using "#".
+The argument supplied should determine the width/height of square.
+"""
+
+
 def print_square(size):
-    try:
-        if not isinstance(size, (int, float)) or isinstance(size, bool):
-            raise TypeError("size must be an integer")
-        if isinstance(size, float) and size < 0:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+    """Print a perfect square given a valid int or float argument.
+    """
+    if not isinstance(size, (int, float)) or isinstance(size, bool):
+        raise TypeError("size must be an integer")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
 
 
-        for i in range(int(size)):
-            print("#" * int(size))
-
-    except Exception as err:
-        print(err)
+    for i in range(int(size)):
+        print("#" * int(size))
