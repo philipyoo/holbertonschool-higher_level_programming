3ae40d0e9f19b06c43a30db3ec34e37365f91262
philipyoo <philipyoo10@gmail.com>
2017-01-19 01:08:37 +0000

    Edit 0x06-3 and relevant tests

--- 0x06-python-test_driven_development/3-say_my_name.py
+++ 0x06-python-test_driven_development/3-say_my_name.py
@@ -10,11 +10,8 @@ It prints "My name is (first) (last)" where (first) and (last) are the args.
 def say_my_name(first_name, last_name=""):
     """Print My name is (first) (last) if given, else print error.
     """
-    try:
-        if not isinstance(first_name, str):
-            raise TypeError("first_name must be a string")
-        if not isinstance(last_name, str):
-            raise TypeError("last_name must be a string")
-        print("My name is {} {}".format(first_name, last_name))
-    except TypeError as err:
-        print(err)
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
