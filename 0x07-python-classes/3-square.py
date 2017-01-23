341fa97ffe715ad6ddbdd12da84e86091d8448f6
philipyoo <philipyoo10@gmail.com>
2017-01-23 18:56:43 +0000

    Add solution for 0x07-3

---
+++ 0x07-python-classes/3-square.py
@@ -0,0 +1,11 @@
+class Square:
+    """A class that defines a square by size and can compute area"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
