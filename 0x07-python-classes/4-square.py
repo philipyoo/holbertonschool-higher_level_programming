3a9d2763166d35401f51028daf70023e6c0e5302
philipyoo <philipyoo10@gmail.com>
2017-01-23 19:54:22 +0000

    Add solution for 0x07-4 and tests for it

---
+++ 0x07-python-classes/4-square.py
@@ -0,0 +1,19 @@
+class Square:
+    """A class that defines a square by size and can compute area"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
