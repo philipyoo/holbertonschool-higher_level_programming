7c7043755666aba26a8ea8026e340a54ecd9008d
philipyoo <philipyoo10@gmail.com>
2017-01-23 20:21:49 +0000

    Add solution for 0x07-6

---
+++ 0x07-python-classes/6-square.py
@@ -0,0 +1,43 @@
+class Square:
+    """A class that defines a square by size, which defaults 0.
+    Square can also get area, and print square using '#'.
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple or len(value) != 2 or \
+           not all([type(i) == int for i in value]):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size is 0:
+            print("")
+            return
+        for i in range(self.__position[1]):
+            print("")
+        for i in range(self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
