af2ecfed52fcf21d76fafa4c14e715fdc372c0ac
philipyoo <philipyoo10@gmail.com>
2017-01-24 07:06:51 +0000

    Edit 0x07-10

--- 0x07-python-classes/103-magic_class.py
+++ 0x07-python-classes/103-magic_class.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-import dis
-import math
-
-
 class MagicClass:
     def __init__(self, radius):
         self.__radius = 0
@@ -20,5 +16,3 @@ class MagicClass:
 
     def circumference(self):
         return 2 * math.pi * self.__radius
-
-dis.dis(MagicClass)
