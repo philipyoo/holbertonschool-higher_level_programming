2cb81625424d75735409ea6fbf512e3ae6f516d8
philipyoo <philipyoo10@gmail.com>
2017-01-24 06:32:52 +0000

    Add docs for 0x07-3

--- 0x07-python-classes/3-square.py
+++ 0x07-python-classes/3-square.py
@@ -1,3 +1,13 @@
+#!/usr/bin/python3
+"""
+This is the "Square"  module.
+
+This module provides a simple Square class with initialize size.
+Defaults size to 0. Raise error on invalid size inputs.
+Method area returns size of area of the square.
+"""
+
+
 class Square:
     """A class that defines a square by size and can compute area"""
     def __init__(self, size=0):
