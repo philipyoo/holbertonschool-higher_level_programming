5261b12207fa36647d78b1cf0d3a8685fe51927f
philipyoo <philipyoo10@gmail.com>
2017-01-24 06:31:32 +0000

    Add doc for 0x07-2

--- 0x07-python-classes/2-square.py
+++ 0x07-python-classes/2-square.py
@@ -1,3 +1,12 @@
+#!/usr/bin/python3
+"""
+This is the "Square"  module.
+
+This module provides a simple Square class with initialize size.
+Size defaults to 0. Raise errors on invalid inputs.
+"""
+
+
 class Square:
     """A class that defines a square by size"""
     def __init__(self, size=0):
