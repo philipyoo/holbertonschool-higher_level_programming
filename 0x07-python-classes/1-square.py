92b19ceb9d5348b89995bc23757818767dd84e55
philipyoo <philipyoo10@gmail.com>
2017-01-24 06:30:24 +0000

    Add module doc for 0x07-1

--- 0x07-python-classes/1-square.py
+++ 0x07-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This is the "Square"  module.
+
+This module provides a simple Square class with initialize size.
+"""
+
 
 class Square:
     """A class that defines a Square by size"""
