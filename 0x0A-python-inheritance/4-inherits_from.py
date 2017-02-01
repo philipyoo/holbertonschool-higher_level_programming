7ca13deb1d16619e3fa62a575f497201c25c6180
philipyoo <philipyoo10@gmail.com>
2017-02-01 00:48:11 +0000

    Edit solutions for 0x0A-4 and 8

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,3 +2,4 @@
 def inherits_from(obj, a_class):
     if (type(obj) != a_class):
         return issubclass(type(obj), a_class)
+    return False
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -11,7 +11,7 @@ class BaseGeometry:
 
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
-        if self.integer_validator("width", width):
-            self.__width = width
-        if self.integer_validator("height", height):
-            self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
