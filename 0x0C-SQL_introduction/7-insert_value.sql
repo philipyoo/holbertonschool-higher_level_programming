74b3dbe45baa68acbea17e1bb0cbe2180bb1c5bb
philipyoo <philipyoo10@gmail.com>
2017-02-22 20:48:54 +0000

    Add solution for 0x0C-7

---
+++ 0x0C-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- Insert a new row in 'first_table' from db 'hbtn_0c_0'
+-- id == 89
+-- name == Holberton School
+-- db name will be passed as arg to mysql cmd
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
