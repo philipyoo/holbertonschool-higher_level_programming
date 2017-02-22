78b3eb406487119b738e6738dfc09ab9ecbdfb96
philipyoo <philipyoo10@gmail.com>
2017-02-22 22:48:12 +0000

    Add solution for 0x0C-102

---
+++ 0x0C-SQL_introduction/102-top_city.sql
@@ -0,0 +1,9 @@
+-- Using temperature data set from ex.18
+-- Display top 3 cities by temperature during July and August
+-- ordered by temperature descending
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
