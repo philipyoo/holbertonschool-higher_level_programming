6961847d2a44d67986e72e7db004669f4b799f31
philipyoo <philipyoo10@gmail.com>
2017-02-22 20:36:02 +0000

    Add solution for 0x0C-5

---
+++ 0x0C-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Print the full description of 'first_table' from db 'hbtn_0c_0'
+-- db name will be passed as an arg to mysql cmd
+-- Not allowed to use DESCRIBE or EXPLAIN
+SHOW CREATE TABLE first_table;
