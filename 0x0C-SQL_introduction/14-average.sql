a21a9bdd9fb0325b0938822c81811e023fd61c9e
philipyoo <philipyoo10@gmail.com>
2017-02-22 21:31:33 +0000

    Add solution for 0x0C-14

---
+++ 0x0C-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Computer score average of all records in 'second_table' of 'hbtn_0c_0'
+-- Result column name should be 'average'
+-- db name will be passed as arg of mysql cmd
+SELECT AVG(score) AS average FROM second_table;
