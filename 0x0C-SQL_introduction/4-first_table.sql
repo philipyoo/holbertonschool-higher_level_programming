bef9c36a699d4e93e793e5b5531e144a18433077
philipyoo <philipyoo10@gmail.com>
2017-02-22 20:11:18 +0000

    Add solution for 0x0C-4

---
+++ 0x0C-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Create a table called 'first_table' in db 'hbtn_0c_0'
+-- 'first_table' should have (id INT) and (name VARCHAR(256))
+-- db name will be passed as argument of mysql cmd
+-- If table 'first_table' already exists, should not fail.
+-- Not allowed to use SELECT or SHOW
+CREATE TABLE IF NOT EXISTS first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(256));
