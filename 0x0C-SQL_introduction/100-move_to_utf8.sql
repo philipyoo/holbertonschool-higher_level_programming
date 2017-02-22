7b5e1739c528a7ff5a263a52d9034d6c0dea99ce
philipyoo <philipyoo10@gmail.com>
2017-02-22 23:19:30 +0000

    Edit comments for 0x0C-17

--- 0x0C-SQL_introduction/100-move_to_utf8.sql
+++ 0x0C-SQL_introduction/100-move_to_utf8.sql
@@ -1,16 +1,16 @@
 -- Convert 'hbtn_0c_0' to utf8mb4, collate utf8mb4_unicode_ci
 -- Convert db 'hbtn_0c_0'
--- Convert table 'first_table'
--- Convert field 'name' in 'first_table'
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
+-- Convert table 'first_table'
 USE hbtn_0c_0;
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
+-- Convert field 'name' in 'first_table'
 USE hbtn_0c_0;
 ALTER TABLE first_table
 CHANGE name name VARCHAR(256)
