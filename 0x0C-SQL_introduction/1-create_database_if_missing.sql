f6ab9ed7849d6db772e0586dbd81d85dacfdbdcd
philipyoo <philipyoo10@gmail.com>
2017-02-22 18:50:10 +0000

    Add solution for 0x0C-1

---
+++ 0x0C-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- Write a script that creates the database 'hbtn_0c_0' in MySQL server
+-- If database already exists, the script should not fail
+-- Not allowed to use SELECT or SHOW
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
