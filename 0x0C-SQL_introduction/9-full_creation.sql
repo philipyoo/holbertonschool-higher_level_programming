105a1c8f513ce202ab7f2d274b3484531c906a71
philipyoo <philipyoo10@gmail.com>
2017-02-22 23:34:13 +0000

    Edit comments for 0x0C-9

--- 0x0C-SQL_introduction/9-full_creation.sql
+++ 0x0C-SQL_introduction/9-full_creation.sql
@@ -2,8 +2,9 @@
 -- Add descriptions (id INT), (name VARCHAR(256)), (score INT)
 -- db name will be passed as arg to mysql cmd
 -- Not allowed to use SELECT or SHOW
--- Script should create 4 records
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+
+-- Script should create 4 records
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
