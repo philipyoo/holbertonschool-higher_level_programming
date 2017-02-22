fa90a7a76a67dd0cea9b83e46599040d5efc1358
philipyoo <philipyoo10@gmail.com>
2017-02-22 21:16:01 +0000

    Add solution for 0x0C-9

---
+++ 0x0C-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- Create table 'second_table' in db 'hbtn_0c_0'
+-- Add descriptions (id INT), (name VARCHAR(256)), (score INT)
+-- db name will be passed as arg to mysql cmd
+-- Not allowed to use SELECT or SHOW
+-- Script should create 4 records
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
