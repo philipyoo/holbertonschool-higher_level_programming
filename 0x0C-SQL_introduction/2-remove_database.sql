4eac3955d2902d1c1175aa8b87da1f3ee349f6f5
philipyoo <philipyoo10@gmail.com>
2017-02-23 01:15:53 +0000

    Rename 0x0C-2

--- 0x0C-SQL_introduction/2-remove_database.sql
+++ 0x0C-SQL_introduction/2-remove_database.sql
@@ -1,4 +1,4 @@
 -- Delete the database 'hbtn_0c_0'
 -- If 'hbtn_0c_0' doesn't exist, script should not fail
 -- Not allowed to use SELECT or SHOW
-DROP DATABASE IF EXISTS hbtn_0c_0
+DROP DATABASE IF EXISTS hbtn_0c_0;
