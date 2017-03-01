1115ec756cf32fa6fd2db285e588694f2f91f23e
philipyoo <philipyoo10@gmail.com>
2017-03-01 06:29:32 +0000

    Edit solution for 0x0D-101

--- 0x0D-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0D-SQL_more_queries/101-not_a_comedy.sql
@@ -10,4 +10,5 @@ WHERE s.title NOT IN (
       INNER JOIN tv_show_genres m ON s.id = m.show_id
       INNER JOIN tv_genres g ON m.genre_id = g.id
       WHERE g.name = 'Comedy'
-);
+)
+ORDER BY s.title ASC;
