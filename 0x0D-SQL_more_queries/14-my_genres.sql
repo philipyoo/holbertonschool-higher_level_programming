863b797d017e225ec6eb6944e8668237c79f849e
philipyoo <philipyoo10@gmail.com>
2017-03-01 01:49:13 +0000

    Edit solution for 0x0D-14

--- 0x0D-SQL_more_queries/14-my_genres.sql
+++ 0x0D-SQL_more_queries/14-my_genres.sql
@@ -7,4 +7,5 @@ SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres m ON tv_genres.id = m.genre_id
 INNER JOIN tv_shows s ON m.show_id = s.id
-WHERE s.title = 'Dexter';
+WHERE s.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
