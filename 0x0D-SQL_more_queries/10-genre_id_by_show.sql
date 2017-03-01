fdbe63fcd74d0bcf35c05179f121826cc4f86ca6
philipyoo <philipyoo10@gmail.com>
2017-03-01 02:18:23 +0000

    Refactor solution 10

--- 0x0D-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0D-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,5 +5,5 @@
 -- You can only use one SELECT statement
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
