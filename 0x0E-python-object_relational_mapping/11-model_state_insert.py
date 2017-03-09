b8863f7843c48ddc0fc674d899f96a9f1c976cf4
philipyoo <philipyoo10@gmail.com>
2017-03-09 05:56:56 +0000

    Add solution for 0x0E-11

---
+++ 0x0E-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# Add the State object "Louisiana" to db 'hbtn_0e_6_usa'
+# Print the new 'states.id' after creation
+# Script should take 3 args: username, pw, and db name
+# Must use SQLAlchemy
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.sql import select
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    l = State(name='Louisiana')
+    session.add(l)
+    session.commit()
+
+    print(l.id)
