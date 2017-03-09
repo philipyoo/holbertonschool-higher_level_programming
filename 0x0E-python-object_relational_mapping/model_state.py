0ecb13512e9d2563695c8583d94431551e0d3223
philipyoo <philipyoo10@gmail.com>
2017-03-09 00:42:02 +0000

    Add solution for 0x0E-6

---
+++ 0x0E-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Write a file that contains class defns of a State and an instance
+# Base = delcarative_base()
+# State class:
+# inherits from Base class, links to MySQL table 'states',
+# has columns id and name
+# Must use SQLAlchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True,
+                nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
