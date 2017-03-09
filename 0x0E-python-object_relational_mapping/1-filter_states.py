9988be57a7579d1c18fab1cfe83f1b52e9da7326
philipyoo <philipyoo10@gmail.com>
2017-03-09 06:59:00 +0000

    Edit solution for 0x0E-1

--- 0x0E-python-object_relational_mapping/1-filter_states.py
+++ 0x0E-python-object_relational_mapping/1-filter_states.py
@@ -19,7 +19,8 @@ if __name__ == "__main__":
     nStates = cur.fetchall()
 
     for state in nStates:
-        print(state)
+        if (state[1][0] == 'N'):
+            print(state)
 
     cur.close()
     db.close()
