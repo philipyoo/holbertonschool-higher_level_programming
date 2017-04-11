cfe5a11c179d38c484794149e7f97c2d2617cb15
philipyoo <philipyoo10@gmail.com>
2017-04-11 20:48:45 +0000

    Move all print statements in with block for solution 0

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -3,9 +3,9 @@
 import urllib.request
 
 if __name__ == "__main__":
-    print("Body response:")
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
         content = res.read()
+        print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
