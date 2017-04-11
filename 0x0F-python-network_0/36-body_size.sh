13384490f8b68cc8a6325ef600db6ce365d8dd03
philipyoo <philipyoo10@gmail.com>
2017-04-11 03:48:33 +0000

    Edit solution for 0x0F-36

--- 0x0F-python-network_0/36-body_size.sh
+++ 0x0F-python-network_0/36-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to take a URL, send request to URL, and display size of body response
-curl -sI "$1" | grep Content-Length
+curl -sI "$1" | grep Content-Length | cut -d " " -f2
