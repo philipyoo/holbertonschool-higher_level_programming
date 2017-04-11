9063ea237c016e87e911f0722f009689ef102cfd
philipyoo <philipyoo10@gmail.com>
2017-04-11 05:58:13 +0000

    Remove extra comment line in 42

--- 0x0F-python-network_0/100-status_code.sh
+++ 0x0F-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Send a request to a URL and display only status code of response with restrictions
-# Cannot use pipes, redirections, bash && or ;
 curl -s -o /dev/null -w "%{http_code}" "$1"
