29ecdbf947a8563c31cb2a395bc4a4f95ead78bb
philipyoo <philipyoo10@gmail.com>
2017-04-11 21:23:04 +0000

    Edit solution 2 to comply with pep8

--- 0x10-python-network_1/2-post_email.py
+++ 0x10-python-network_1/2-post_email.py
@@ -8,7 +8,7 @@ import urllib.request
 import urllib.parse
 
 if __name__ == "__main__":
-    values = {'email' : sys.argv[2]}
+    values = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(values)
     data = data.encode('utf-8')
     req = urllib.request.Request(sys.argv[1], data)
