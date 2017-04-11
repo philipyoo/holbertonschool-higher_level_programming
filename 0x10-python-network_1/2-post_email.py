90e8a9168edba674dd3dd538909997b9c3f4df19
philipyoo <philipyoo10@gmail.com>
2017-04-11 21:22:09 +0000

    Add solution for 0x10-2

---
+++ 0x10-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Take in a URL and email, send POST request, and display body of response
+decoded in utf-8
+"""
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    values = {'email' : sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as res:
+        print(res.read().decode('utf-8'))
