8c65b0e80143a3da39925183d9e653eff123c754
philipyoo <philipyoo10@gmail.com>
2017-04-27 19:23:28 +0000

    Edit solution for 0x11-2

--- 0x11-javascript-warm_up/2-arguments.js
+++ 0x11-javascript-warm_up/2-arguments.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
-console.log(process.argv.length - 2);
+const numArgs = process.argv.length - 2;
+if (numArgs === 0) {
+    console.log("No argument")
+} else if (numArgs === 1) {
+    console.log("Argument found")
+} else {
+    console.log("Arguments found")
+}
