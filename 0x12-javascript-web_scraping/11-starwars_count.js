fb3dab6260e709d43fed0a74bc462d86fad5ca66
philipyoo <philipyoo10@gmail.com>
2017-04-29 04:35:05 +0000

    Add solution for 0x12-11

---
+++ 0x12-javascript-web_scraping/11-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+var request = require('request');
+
+const url = process.argv[2];
+
+request(url, (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  let count = 0;
+  let parsedBody = JSON.parse(body).results;
+
+  for (let i = 0; i < parsedBody.length; i++) {
+    let a = parsedBody[i].characters.find((c) => {
+      return c.match(/18/);
+    });
+    if (a !== undefined) {
+      count++;
+    }
+  }
+
+  console.log(count);
+});
