c500b9846c846b19d400fd8bf6b998311500f60e
philipyoo <philipyoo10@gmail.com>
2017-04-29 02:20:55 +0000

    Edit solution for 0x12-8

--- 0x12-javascript-web_scraping/8-writeme.js
+++ 0x12-javascript-web_scraping/8-writeme.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 const file = process.argv[2] || '';
 const data = process.argv[3] || '';
 
-fs.writeFile(file, data, (err) => {
+fs.writeFile(file, data, 'utf8', (err) => {
   if (err) {
     return console.log(err);
   }
