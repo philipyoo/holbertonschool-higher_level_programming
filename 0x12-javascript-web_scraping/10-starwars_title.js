1aa96b66291ee1d91c4e70b13de0825780e5d170
philipyoo <philipyoo10@gmail.com>
2017-04-29 04:20:32 +0000

    Add solution for 0x12-10

---
+++ 0x12-javascript-web_scraping/10-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+var request = require('request');
+
+const episodeId = process.argv[2];
+
+if (parseInt(episodeId) < 8) {
+  const url = 'http://swapi.co/api/films/' + episodeId;
+
+  request(url, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(JSON.parse(body).title);
+  });
+}
