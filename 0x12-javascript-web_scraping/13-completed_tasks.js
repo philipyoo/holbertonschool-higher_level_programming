3e722c696c76e76691bdba8e70a8cdeeb81bf556
philipyoo <philipyoo10@gmail.com>
2017-04-29 05:13:57 +0000

    Add solution for 0x12-13

---
+++ 0x12-javascript-web_scraping/13-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+var request = require('request');
+
+const url = process.argv[2] || '';
+let storage = {};
+
+request(url, (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  let parsedBody = JSON.parse(body);
+
+  for (let i = 0; i < parsedBody.length; i++) {
+    let tmp = parsedBody[i].userId;
+    if (parsedBody[i].completed) {
+      storage[tmp] = (storage[tmp] || 0) + 1;
+    }
+  }
+
+  console.log(storage);
+});
