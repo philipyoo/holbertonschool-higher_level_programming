eaf8b3b7027cac3295b5db223691e0bd844cd511
philipyoo <philipyoo10@gmail.com>
2017-04-29 02:39:30 +0000

    Add solution for 0x12-9

---
+++ 0x12-javascript-web_scraping/9-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+var request = require('request');
+
+const url = process.argv[2] || '';
+
+request(url, (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('code: ' + res.statusCode);
+});
