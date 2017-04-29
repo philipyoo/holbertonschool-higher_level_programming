21eabb52d95f9f5fcb030db29fc88a4fa4c8ab7e
philipyoo <philipyoo10@gmail.com>
2017-04-29 04:45:09 +0000

    Add solution for 0x12-12

---
+++ 0x12-javascript-web_scraping/12-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+var fs = require('fs');
+var request = require('request');
+
+const url = process.argv[2] || '';
+const filePath = process.argv[3];
+
+request(url, (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  fs.writeFile(filePath, body, 'utf8', (err) => {
+    if (err) {
+      return console.log(err);
+    }
+  });
+});
