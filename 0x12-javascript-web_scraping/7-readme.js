6d1650a2dfc6f093304eabff0b8d8cbfde71b5d5
philipyoo <philipyoo10@gmail.com>
2017-04-29 02:21:13 +0000

    Edit solution for 0x12-7

--- 0x12-javascript-web_scraping/7-readme.js
+++ 0x12-javascript-web_scraping/7-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 var fs = require('fs');
 
-var file = process.argv[2] || '';
+const file = process.argv[2] || '';
 
-fs.readFile(file, 'utf8', function (err, data) {
+fs.readFile(file, 'utf8', (err, data) => {
   if (err) {
     return console.log(err);
   }
