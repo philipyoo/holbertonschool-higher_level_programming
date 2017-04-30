280e627c02370ea70406fc1f67436cf7b6962a43
philipyoo <philipyoo10@gmail.com>
2017-04-30 22:07:03 +0000

    Fix require statement on 0x12-5

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
-const Rectangle = require('./5-rectangle.js').Rectangle;
+const Rectangle = require('./4-rectangle.js').Rectangle;
 
 function Square (size) {
   Rectangle.call(this, size, size);
