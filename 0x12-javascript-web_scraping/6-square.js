e7880d2bcccacf157ecea55ccff5f544f9df05cf
philipyoo <philipyoo10@gmail.com>
2017-04-30 22:22:10 +0000

    Edit solution for 0x12-6

--- 0x12-javascript-web_scraping/6-square.js
+++ 0x12-javascript-web_scraping/6-square.js
@@ -1,36 +1,12 @@
 #!/usr/bin/node
-function Rectangle (w, h) {
-  if (w > 0 && h > 0) {
-    this.width = w;
-    this.height = h;
-  }
-  this.print = function () {
-    for (let i = 0; i < this.height; i++) {
-      console.log(('X').repeat(this.width));
-    }
-  };
-  this.rotate = function () {
-    [this.width, this.height] = [this.height, this.width];
-  };
-  this.double = function () {
-    this.width *= 2;
-    this.height *= 2;
-  };
-}
-
-function Square (size) {
-  Rectangle.call(this, size, size);
-}
+const Square = require('./5-square.js').Square;
 
 Square.prototype.charPrint = function (c) {
   if (c === undefined) {
-    for (let i = 0; i < this.height; i++) {
-      console.log(('X').repeat(this.width));
-    }
-  } else {
-    for (let i = 0; i < this.height; i++) {
-      console.log((c).repeat(this.width));
-    }
+    c = 'X';
+  }
+  for (let i = 0; i < this.height; i++) {
+    console.log((c).repeat(this.width));
   }
 };
 
