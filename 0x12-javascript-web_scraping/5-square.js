47ed98de941ea34e774fc7e3a63d841de35722a7
philipyoo <philipyoo10@gmail.com>
2017-04-30 22:03:28 +0000

    Edit solution for 0x12-5

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -1,5 +1,23 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle.js').Rectangle;
+
+function Rectangle (w, h) {
+  if (w > 0 && h > 0) {
+    this.width = w;
+    this.height = h;
+  }
+  this.print = function () {
+    for (let i = 0; i < this.height; i++) {
+      console.log(('X').repeat(this.width));
+    }
+  };
+  this.rotate = function () {
+    [this.width, this.height] = [this.height, this.width];
+  };
+  this.double = function () {
+    this.width *= 2;
+    this.height *= 2;
+  };
+}
 
 function Square (size) {
   Rectangle.call(this, size, size);
