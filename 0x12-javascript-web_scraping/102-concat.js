32f2618e0712771b92c22abd1ec4c58193b75f3c
philipyoo <philipyoo10@gmail.com>
2017-05-01 16:15:28 +0000

    Edit solution for 102

--- 0x12-javascript-web_scraping/102-concat.js
+++ 0x12-javascript-web_scraping/102-concat.js
@@ -5,12 +5,12 @@ let fileA = process.argv[2];
 let fileB = process.argv[3];
 let fileC = process.argv[4];
 
-if (!fileA || !fileB || !fileC) {
-  console.log('nope');
-} else {
+try {
   [fileA, fileB].forEach((f) => {
     fs.readFileSync(f).toString().trim().split('\n').forEach((line) => {
       fs.appendFileSync(fileC, line.toString() + '\n');
     });
   });
+} catch (err) {
+  console.log(err);
 }
