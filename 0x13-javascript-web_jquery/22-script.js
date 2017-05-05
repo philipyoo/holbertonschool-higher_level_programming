a206250ded542c85d016a22fb6eca08b26c737db
philipyoo <philipyoo10@gmail.com>
2017-05-05 00:31:25 +0000

    Edit solution for 0x13-22

--- 0x13-javascript-web_jquery/22-script.js
+++ 0x13-javascript-web_jquery/22-script.js
@@ -1,13 +1,5 @@
-#!/usr/bin/node
-const request = require('request');
-
-request('http://swapi.co/api/films?format=json', (err, res, body) => {
-  if (err) {
-    return console.log(err);
-  }
-
-  let data = JSON.parse(body).results;
-  for (let i = 0; i < data.length; i++) {
-    $('ul#list_movies').append('<li>' + data[i].title + '</li>');
+$.get('http://swapi.co/api/films?format=json', (data) => {
+  for (let i = 0; i < data.results.length; i++) {
+    $('ul#list_movies').append('<li>' + data.results[i].title + '</li>');
   }
 });
