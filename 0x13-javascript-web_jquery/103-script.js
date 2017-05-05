e028728028756ea0c1076dbd36a2f88f64829263
philipyoo <philipyoo10@gmail.com>
2017-05-05 00:36:51 +0000

    Edit solution for 0x13-103

--- 0x13-javascript-web_jquery/103-script.js
+++ 0x13-javascript-web_jquery/103-script.js
@@ -1,7 +1,4 @@
-#!/usr/bin/node
 $(document).ready(function () {
-  const request = require('request');
-
   $('INPUT#city_search').keypress(function (e) {
     if (e.which === 13) {
       $('#btn_search').click();
@@ -14,12 +11,8 @@ $(document).ready(function () {
 
     let url = 'https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + cityName + '%22)&format=json';
 
-    request(url, (err, res, body) => {
-      if (err) {
-        return console.log(err);
-      }
-
-      $('DIV#wind_speed').text(JSON.parse(body).query.results.channel.wind.speed);
+    $.get(url, (data) => {
+      $('DIV#wind_speed').text(data.query.results.channel.wind.speed);
     });
   });
 });
