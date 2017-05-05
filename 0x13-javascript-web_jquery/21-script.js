27ba53570a8c98b4d3eedccbd10b9ff5542a743d
philipyoo <philipyoo10@gmail.com>
2017-05-05 00:20:27 +0000

    Edit solution for 0x13-21

--- 0x13-javascript-web_jquery/21-script.js
+++ 0x13-javascript-web_jquery/21-script.js
@@ -1,9 +1,3 @@
-#!/usr/bin/node
-const request = require('request');
-
-request('http://swapi.co/api/people/5/?format=json', (err, res, body) => {
-  if (err) {
-    return console.log(err);
-  }
-  $('DIV#character').text(JSON.parse(body).name);
+$.get('https://swapi.co/api/people/5/?format=json', (data) => {
+  $('DIV#character').text(data.name);
 });
